7b476fd2c8a8075638bd9ed2fd8974e3bd63966f
auxfuse <Anthony.obrien2@hse.ie>
2019-09-07 11:15:48 +0100

    Created event listener function and added function stop

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,1 +1,6 @@
-{\rtf1}
+window.addEventListener("keydown", function(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+    //If no audio key is hit, stop function from running.
+    if(!audio) return;
+});

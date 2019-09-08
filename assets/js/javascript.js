6a7a0568644b49829d62cf41809f50fde001a683
auxfuse <Anthony.obrien2@hse.ie>
2019-09-08 12:09:49 +0100

    created playSound function and removed from eventListener, pushed eventlistener to bottom

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,6 +1,7 @@
-window.addEventListener("keydown", function(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+function playSound(e){
 
+    //Local Variables for event Listener.
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
     //If no audio key is hit, stop function from running.
@@ -12,8 +13,21 @@ window.addEventListener("keydown", function(e){
     //Call function to play on `keydown` of event Listener.
     audio.play();
 
+    //When key is clicked, add class of playing to element to highlight to User of successful click.
     key.classList.add("playing");
-});
+};
 
+function removeTransition(e) {
+
+    if(e.propertyName !== "transform") return;
+
+    this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playSound);
 
 
